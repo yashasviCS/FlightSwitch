be0a1c96867eedd05c663ce7a0e076c85cd1016c
Michael Zetune <michael@zetune.com>
2019-11-02 16:55:43 -0500

    env var

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-# from fastapi import FastAPI
-#
-# app = FastAPI()
-#
-#
-# @app.get("/hello")
-# def hello_world():
-#     return {"Hello": "World"}
-
 from flask import Flask
 app = Flask(__name__)
-
+import os
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ['PORT']))

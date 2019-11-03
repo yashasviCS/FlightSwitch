8262edb7575a5ca2062790d693c2b21285538b59
Yashasvi Madan <yashasvimadan@wireless-10-147-63-129.public.utexas.edu>
2019-11-02 22:45:46 -0500

    switch bookings method

--- main.py
+++ main.py
@@ -21,6 +21,49 @@ def create_test_user():
     usersCollection.insert_one({"username": "testusername", "password": "bad_password" })
     return "Successfully inserted test user"
 
+@app.route('/switch_bookings')
+def switch(idA, idB):
+    switch1 = bookingsCollection.find_one({"_id":idA})
+    switch2 = bookingsCollection.find_one({"_id":idB})
+    switch1Username = switch1["username"]
+    switch2Username = switch2["username"]
+    test_db.bookingsCollection.update_one({
+        '_id': switch1['_id']
+    }, {
+        '$set': {
+            'username': switch2["username"]
+        }
+    }, upsert=False)
+    test_db.bookingsCollection.update_one({
+        '_id': switch2['_id']
+    }, {
+        '$set': {
+            'username': switch1Username
+        }
+    }, upsert=False)
+    user1 = usersCollection.find_one({"username": switch1Username})
+    user2 = usersCollection.find_one({"username": switch2Username})
+    user1BookingId = user1["booking_ids"]
+    user2BookingId = user2["booking_ids"]
+    for booking_id in user1BookingId:
+        if booking_id == idA:
+            test_db.usersCollection.update_one({
+                '_id': user1["_id"]
+            }, {
+                '$set': {
+                    'booking_id': idB
+                }
+            }, upsert=False)
+    for booking_id in user2BookingId:
+        if booking_id == idB:
+            test_db.usersCollection.update_one({
+                '_id': user2["_id"]
+            }, {
+                '$set': {
+                    'booking_id': idA
+                }
+            }, upsert=False)
+
 # usersCollection.close()
 # bookingsCollection.close()
 # flightsCollection.close()
